e88a249124c3df640b21db651414e2e75f246d80
Diego Noceda <diego.noceda@udc.es>
2017-06-24 20:10:53 +0200

    igual era bueno haber ejecutado la version buena antes

--- arduino/testserver/server.py
+++ arduino/testserver/server.py
@@ -7,17 +7,22 @@
 import serial
 import requests
 import time
+import sys
+import json
 
 """
 JUE == Cafrada
 """
 
-ser = serial.Serial("/dev/ttyACM0", 9600)
+port = sys.argv[1]
+
+ser = serial.Serial("/dev/tty"+port, 9600)
 
 with open("server_cfg.json", "r") as f:
     cfg = json.load(f)
-    READ_API_KEY = cfg['API_KEY']['WRITE']
-    WRITE_API_KEY = cfg['API_KEY']['READ']
+    WRITE_API_KEY = cfg['API_KEY']['WRITE']
+    READ_API_KEY = cfg['API_KEY']['READ']
+
 
 def update_thingspeak(data):
     r = requests.get("https://api.thingspeak.com/update?api_key="+WRITE_API_KEY+"&field1="+data["soil"]+"&field2="+data["light"]+"&field3="+data["temp"]+"&field4="+data["timestamp"]) #JUE
