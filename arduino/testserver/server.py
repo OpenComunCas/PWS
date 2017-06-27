989db34d42f3c11348898a0b6ace941250db857a
Fynardo <dfynar@gmail.com>
2017-06-27 22:59:42 +0200

    nuevo canal de thingspeak para pruebas

--- arduino/testserver/server.py
+++ arduino/testserver/server.py
@@ -20,20 +20,26 @@ ser = serial.Serial("/dev/tty"+port, 9600)
 
 with open("server_cfg.json", "r") as f:
     cfg = json.load(f)
-    WRITE_API_KEY = cfg['API_KEY']['WRITE']
-    READ_API_KEY = cfg['API_KEY']['READ']
+    MEASURES_WRITE_KEY = cfg['MEASURES_KEY']['WRITE']
+    MEASURES_READ_KEY = cfg['MEASURES_KEY']['READ']
 
+    ACTIONS_WRITE_KEY = cfg['ACTIONS_KEY']['WRITE']
+    ACTIONS_READ_KEY = cfg['ACTIONS_KEY']['READ']
 
-def update_thingspeak(data):
-    r = requests.get("https://api.thingspeak.com/update?api_key="+WRITE_API_KEY+"&field1="+data["soil"]+"&field2="+data["light"]+"&field3="+data["temp"]+"&field4="+data["timestamp"]) #JUE
+def update_measures(data):
+    r = requests.get("https://api.thingspeak.com/update?api_key="+MEASURES_WRITE_KEY+"&field1="+data["soil"]+"&field2="+data["light"]+"&field3="+data["temp"]+"&field4="+data["timestamp"]) #JUE
+    print(r.text)
 
+def update_actions(action):
+    timestamp = str(int(time.time()))
+    r = requests.get("https://api.thingspeak.com/update?api_key="+ACTIONS_WRITE_KEY+"&field1="+action+"&field2="+timestamp)  # JUE
     print(r.text)
 
 
 while True:
     cmd = ser.readline().decode()
     if cmd == "!action>\r\n": #JUE
-        print(ser.readline())
+        update_actions(ser.readline().decode())
     if cmd == "!data>\r\n": #JUE
         data = {}
         recv = ser.readline().decode()
@@ -43,6 +49,6 @@ while True:
             data['timestamp'] = str(int(time.time())) # JUE
             
         print(data)
-        update_thingspeak(data)
+        update_measures(data)
 
         
